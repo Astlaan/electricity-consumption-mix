2720eb39effb88578d9fb7a45db9b8123e4a69cf
Diogo Valada <diogovalada.7@hotmail.com>
2024-10-27 16:26:49 +0000

    .

--- src/utils.py
+++ src/utils.py
@@ -8,16 +8,5 @@ def validate_inputs(args):
         return False
     return True
 
-def aggregate_results(results: pd.DataFrame) -> pd.DataFrame:
-    if results.empty:
-        return pd.DataFrame(columns=list(PSR_TYPE_MAPPING.values()))
-    
-    # Map PSR types to their descriptions
-    results = results.rename(columns=PSR_TYPE_MAPPING)
-    
-
-    return results
-
-
 def get_active_psr_in_dataframe(df):
     return [col for col in df.columns if col in PSR_TYPE_MAPPING]
