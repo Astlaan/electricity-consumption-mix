35605a42b7e3309b9370f66dcf32294b8af87635
Diogo Valada <diogovalada.7@hotmail.com>
2025-05-30 17:15:21 +0100

    updated svelte config to use mdsvex

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,41 +1,22 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
-import { mathsvex } from 'mathsvex';
 import remarkMath from 'remark-math';
 import rehypeKatexSvelte from 'rehype-katex-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// extensions: ['.svelte', '.md'], // mdsvex
-	extensions: ['.svelte', '.md', '.math.js', '.math.ts'], // mathsvex
-
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: [
-		vitePreprocess(),
-		mathsvex(),
-		// mdsvex({
-		// 	extensions: ['.md', '.svx'], // Tell mdsvex to only process .md files
-		// 	highlight: {
-		// 		// highlighter: customHighlighter, // You can add a syntax highlighter here if needed
-		// 	},
-		// 	// Configure remark and rehype plugins for math/KaTeX
-		// 	remarkPlugins: [remarkMath],
-		// 	rehypePlugins: [rehypeKatexSvelte],
-		// 	// You can pass KaTeX options here if needed, e.g., displayMode: true for block equations
-		// 	// katex: {
-		// 	//   // See https://katex.org/docs/options.html
-		// 	// }
-		// })
-	],
-
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+    extensions: ['.svelte', '.md'],
+    preprocess: [
+        vitePreprocess(),
+        mdsvex({
+            extensions: ['.md'],
+            remarkPlugins: [remarkMath],
+            rehypePlugins: [rehypeKatexSvelte],
+        })
+    ],
+    kit: {
+        adapter: adapter()
+    }
 };
 
 export default config;
